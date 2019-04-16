67ccbf0350bf3947456777699a392c0d8f730afd
sandyscoloski <39198481+sandyscoloski@users.noreply.github.com>
2019-04-16 16:59:33 -0300

    Add files via upload

---
+++ qrcode_loader.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+from pyzbar import pyzbar
+from PIL import Image
+from io import BytesIO
+import os.path
+import urllib.request
+import cv2 as cv
+
+
+CAMINHO = 'imagens/qrcode_sandy_junior.png'
+TEMP = 'imagens/temp.jpg'
+
+while not os.path.isfile(CAMINHO):
+    print("Couldn't open this file\n")
+    exit()
+    pass
+
+
+if __name__ == '__main__':
+    # load the input imageQRCode
+    image_code = cv.imread(CAMINHO)
+
+    # find the barcodes in the image and decode each of the barcodes
+    barcodes = pyzbar.decode(image_code)
+
+    # loop over the detected barcodes
+    for barcode in barcodes:
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        # print(f'[INFO] Found {barcodeType} barcode: {barcodeData}')
+
+    # get the page content from url
+    req = urllib.request.Request(barcodeData)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+
+    # converts the content page from image and save a temp image
+    img = Image.open(BytesIO(the_page))
+    img.save(TEMP)
+
+    image = cv.imread(TEMP)
+    cv.imshow('IMAGE from TEMP', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
